799a035f4eaa24f71246ad93e8ed7fb0866552fb
GoranYankov <goran415@abv.bg>
2018-01-14 11:18:08 +0200

    Edit server port

---
+++ src/app/component/article/singel-article/singel-article.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticleServices } from '../../../core/service/article.service';
+import { SeoServices } from '../../../core/service/seo.service';
+
+@Component({
+  selector: 'app-singel-article',
+  templateUrl: './singel-article.component.html',
+  styleUrls: ['./singel-article.component.css']
+})
+export class SingelArticleComponent implements OnInit {
+  article = {
+    img:'',
+    title:'',
+    url:'',
+    paragraph:[0]
+  }
+  constructor(
+        private router: ActivatedRoute, 
+        private http:ArticleServices,
+        private seo: SeoServices
+      
+      ) { }
+
+  ngOnInit() {
+    window.scrollTo(0, 0)
+    
+    let url = this.router.snapshot.params['url'];
+    this.http.singlArticle({url:url}).subscribe(article=>{
+      if(article['success']) {
+      this.article = article['article'];
+      this.seo.changeTitle(this.article.title);
+      this.seo.addMetaKeys(this.article['keywrods']);
+    }
+
+    })
+    
+  }
+
+}
---
+++ src/app/config/http.config.ts
@@ -0,0 +1,38 @@
+export const httpConfig = {
+    http:'http://164.138.216.56:3001',
+    http2: 'http://freegeoip.net/json/',
+    login:'/login',
+    register: '/register',
+    addProduct: '/product/add',
+    getHomeProducts: '/product/getHomeProducts',
+    getSingelProduct: '/product/getSingleProduct',
+    getAllProduct: '/product/all',
+    deleteProduct: '/product/delete',
+    updateProduct: '/product/update',
+    newOrder: '/orders/new',
+    searchOrder: '/orders/searchOrder',
+    allOrders: '/orders/allOrders',
+    editOrderStatus: '/orders/editOrderStatus',
+    allOrdersToday: '/orders/allOrderToDay',
+    allOrdersThisMonth: '/orders/allOrderToMonth',
+    newCart: '/cart/newCart',
+    getCart: '/cart/getCart',
+    updateCart: '/cart/updateCart',
+    removeProductFromCart: '/cart/delete',
+    
+    //Статии
+    addArticle: '/article/add',
+    getNumOfArticle: '/article/getArticle',
+    getCountOfArticle: '/article/getCount',
+    getSinglArticle: '/article/singlArticle',
+    deleteArticle: '/article/delete',
+    updateArticle: '/article/update',
+
+    //Slider
+    getSlider: '/slider',
+    setSlider: '/slider/saveSlider',
+    remove: '/slider/delete',
+    update: '/slider/update'
+
+
+}
